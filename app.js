a27ea573835daebe85963ce1eb930fd8e8553883
Firefly4266 <johnathon@Icon-Base.(none)>
2017-02-19 15:08:58 -0800

    simple server complete

--- app.js
+++ app.js
@@ -3,5 +3,6 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World!!');
-});
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(1333, '127.0.0.1');

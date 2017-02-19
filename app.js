fbc118923ed4bc3ae4f686eb8d6af22ca750475f
Firefly4266 <johnathon@Icon-Base.(none)>
2017-02-19 15:29:24 -0800

    acced notes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 'use strict';
-
+//bring in the http module
 const http = require('http');
-
+//create a server instance and assign it to a variable
 const server = http.createServer((req, res) => {
+  //first response will be the headers using res.writeHead
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  //content to be displayed on the page 
   res.end('Hello World\n');
+  //add a listen object with a port number (1333) and a host(127.0.0.1 localhost)
 }).listen(1333, '127.0.0.1');

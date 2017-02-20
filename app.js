09a5ba588bff781c0c5623ff73d582c71e9a3db8
Firefly4266 <johnathon@Icon-Base.(none)>
2017-02-19 19:47:28 -0800

    added comments and dynamic injection

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
 
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  let html = fs.readFileSync(__dirname + '/index.htm');
+  //add encoding 'utf8' because fs.readFileSync returns a buffer by default. We need to convert the buffer to a string so we can manipulate it BEFORE we pass it to the response (res).
+  let html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
+  //create a variable that holds a string of data
+  let message = 'Hello World...';
+  //use the .replace method to replace the placeholder {Message} with the variable that was created. This is where we manipulate the string data that we will pass to res.end().
+  html = html.replace('{Message}', message);
   res.end(html);
 
 }).listen(1333, '127.0.0.1');
